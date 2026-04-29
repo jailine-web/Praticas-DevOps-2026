5c2ca6458fb7f89d45db045ff6cc37c06a2b9912
unknown <jay.santana964@gmail.com>
2026-04-29 16:16:04 -0300

    Início do desenvolvimento do código de operações multifuncionais - 2.2

---
+++ utils/script_multifuncional.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------#
+# Script Name: Script_multifuncional
+# Description: Script multifuncional para operações com arquivos
+# Site: https://github.com/jailine-web/Praticas-DevOps-2026
+# Written by: Jailine e Rondinele
+# Maintenance: Jailine e Rondinele
+# ------------------------------------------------------------------------#
+#
+# Usage: 
+#	$ ./script_multifuncional.sh [opção] [argumentos]
+#	$ ./script_multifuncional.sh --menu (para modo interativo)
+#
+# ------------------------------------------------------------------------#
+# Variaveis --------------------------------------------------------------#
+OPERACAO="$1"
+ARG1="$2"
+ARG2="$3"
+
+# fUNCTIONS --------------------------------------------------------------#
