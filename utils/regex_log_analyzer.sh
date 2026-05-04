66697494e466aa7ad943e55db83e6c7ac11b28b8
unknown <jay.santana964@gmail.com>
2026-05-04 12:03:48 -0300

    Finalizando a questão 2.1

--- utils/regex_log_analyzer.sh
+++ utils/regex_log_analyzer.sh
@@ -14,6 +14,15 @@ awk '{print $1}' "$ARQUIVO_LOG" | sort | uniq > "$DIR_SAIDA/1_unique_ips.txt"
 # 2. Métodos HTTP e contagem
 awk -F\" '{print $2}' "$ARQUIVO_LOG" | awk '{print $1}' | sort | uniq -c | sort -nr > "$DIR_SAIDA/2_http_methods.txt"
 
+# 3. Métodos perigosos (DELETE e PUT)
+grep -E '"(DELETE|PUT)' "$ARQUIVO_LOG" > "$DIR_SAIDA/3_dangerous_methods.txt"
+
+# 4. URLs e contagem
+awk -F\" '{print $2}' "$ARQUIVO_LOG" | awk '{print $2}' | sort | uniq -c | sort -nr > "$DIR_SAIDA/4_urls.txt"
+
+# 5. Top 10 IPs
+awk '{print $1}' "$ARQUIVO_LOG" | sort | uniq -c | sort -nr | head -10 > "$DIR_SAIDA/5_top_ips.txt"
+
 
 # 6. Relatório consolidado
 {
@@ -29,6 +38,16 @@ awk -F\" '{print $2}' "$ARQUIVO_LOG" | awk '{print $1}' | sort | uniq -c | sort 
     head -5 "$DIR_SAIDA/2_http_methods.txt"
     echo ""
 
+    echo "Requisições perigosas (DELETE/PUT):"
+    wc -l < "$DIR_SAIDA/3_dangerous_methods.txt"
+    echo ""
+
+    echo "Top 5 URLs acessadas:"
+    head -5 "$DIR_SAIDA/4_urls.txt"
+    echo ""
+
+    echo "Top 5 IPs:"
+    head -5 "$DIR_SAIDA/5_top_ips.txt"
 
 } > "$DIR_SAIDA/report.txt"
 
